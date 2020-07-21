747c789620050586af9447dfa1dd401144788c45
SujithAlla <sphur@DESKTOP-SGAOR3B>
2020-07-21 17:16:24 -0400

    Changed the Description and storename

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -6,8 +6,8 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <h1>Hello, Customers!</h1>
+            <p>Shop these great stores in Southeast Florida</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
