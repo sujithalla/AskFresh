bfb8da9cf359eae1ff586a610b7cab4c0103709a
sriram26 <sriram.sakinala@gmail.com>
2020-07-21 23:50:18 -0500

    Added a new component to test email sending functionality

---
+++ ClientApp/src/components/TestMail.js
@@ -0,0 +1,26 @@
+﻿import React, { Component } from 'react';
+
+export class TestMail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.sendEmail = this.sendEmail.bind(this);
+    }
+
+    sendEmail() {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        };
+        fetch('testmail', requestOptions)
+            .then(response => response.json())
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="btn btn-primary" onClick={this.sendEmail}>Send Email</button>
+            </div>
+        );
+    }
+}
---
+++ Controllers/TestMailController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace AskFresh.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestMailController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult SendEmail()
+        {
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("TheCodeBuzz", "sriram.sakinala@gmail.com"));
+                message.To.Add(new MailboxAddress("TheCodeBuzz", "sriram.sakinala@outlook.com"));
+                message.Subject = "My First Email";
+                message.Body = new TextPart("plain")
+                {
+                    Text = "Email is Working Fine"
+                };
+
+                using (var client = new MailKit.Net.Smtp.SmtpClient())
+                {
+
+                    client.Connect("smtp.gmail.com", 587, false);
+
+                    client.Authenticate("emailaddress@gmail.com", "password");
+                    //SMTP server authentication if needed
+                    // client.Authenticate("xxxx@gmail.com", "xxxxx");
+
+                    client.Send(message);
+
+                    client.Disconnect(true);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Error occured");
+            }
+
+            return Ok(true);
+        }
+    }
+}
